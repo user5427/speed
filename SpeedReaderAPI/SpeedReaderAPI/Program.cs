aa52996033d290e94d37db935cd918be0ebbdb62
tari9925 <tadas.riksas@mif.stud.vu.lt>
2024-09-22 20:35:44 +0300

    Forgot to start DB service in Program.cs, now database is configured correctly.

--- SpeedReaderAPI/SpeedReaderAPI/Program.cs
+++ SpeedReaderAPI/SpeedReaderAPI/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SpeedReaderAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<SpeedreaderDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
 var app = builder.Build();
 
