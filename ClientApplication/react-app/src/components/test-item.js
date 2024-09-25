b52061a6dc165f403e0b199f6259d54510dc348a
tari9925 <tadas.riksas@mif.stud.vu.lt>
2024-09-25 19:42:10 +0300

    Frontend is connected to backend now

--- ClientApplication/react-app/src/components/test-item.js
+++ ClientApplication/react-app/src/components/test-item.js
@@ -5,11 +5,11 @@ const TestItem = (props) => {
     return (
         <>
             <Row>
-                <Col item xs={12} md={2}>
-                    <img src={props.data.coverImage || NoImage} style={{width: 150, height: 150}} />
-                </Col>
                 <Col item xs={12} md={10}>
                     <div><b>{props.data.title}</b></div>
+                </Col>
+                <Col item xs={12} md={2}>
+                    <div>{props.data.categoryTitle}</div>
                 </Col>
                 <Col>
                 <hr/>
--- ClientApplication/react-app/src/components/test-list.js
+++ ClientApplication/react-app/src/components/test-list.js
@@ -4,22 +4,21 @@ const TestList = () => {
     const[tests, setTests] = useState(null)
     const[testCount, setTestCount] = useState(0)
     const[page, setPage] = useState(0)
-
     useEffect(() => {
         // get all tests
         getTests();
     }, [page]) // [] if empty, will load for only the first and only first time
 
     const getTests = () => {
-        fetch(process.env.REACT_APP_API_URL + "/test?pageSize=" + process.env.REACT_APP_PAGING_SIZE + "&pageIndex=" + page)
+        fetch(process.env.REACT_APP_API_URL + "Articles?pageIndex=" + page + "&pageSize=" + process.env.REACT_APP_PAGING_SIZE)
         .then(res => res.json())
         .then(res => {
-            if (res.status === true && res.data.totalCount > 0){
-                setTests(res.data.tests);
-                setTestCount(Math.ceil(res.data.totalCount / process.env.REACT_APP_PAGING_SIZE));
+            if (res.status === true && res.data.articles > 0){
+                setTests(res.data.articleList);
+                setTestCount(Math.ceil(res.data.articles / process.env.REACT_APP_PAGING_SIZE));
             }
 
-            if (res.data.totalCount === 0){
+            if (res.data.articles === 0){
                 alert("There is no test data in the system.");
             }
 
@@ -30,7 +29,7 @@ const TestList = () => {
 
     return (
         <>
-            {tests && tests != [] ? 
+            {tests && tests !== [] ? 
             tests.map((m, i) => <TestItem key={i} data={m}/>) 
             : ""}
         
