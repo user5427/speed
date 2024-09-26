782d7efd3c88436cf930805bf80437b70ed1c4dd
tari9925 <tadas.riksas@mif.stud.vu.lt>
2024-09-26 13:11:45 +0300

    Removed backend duplicate

--- SpeedReaderAPI/SpeedReaderAPI/DTOs/Question/Responses/QuestionResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
-namespace SpeedReaderAPI.DTOs.Question.Responses
-{
-    public class QuestionResponse : QuestionDTO
-    {
-    }
-}
