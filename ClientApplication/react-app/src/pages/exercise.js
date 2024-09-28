96ee80cdbf9b5aced9d8708f55662b1a3cceb1b7
Erelix <adrian.klimasevski@gmail.com>
2024-09-28 22:35:30 +0300

    added the flashing words

--- ClientApplication/react-app/src/pages/exercise.js
+++ ClientApplication/react-app/src/pages/exercise.js
@@ -1,14 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import "../styles/exerciseStyle.css"; 
-
+import { Row, Col, Button } from 'react-bootstrap';
 
 const Exercise = () => {
-    return(
-        <div className='mainContainer'>
-            <div className='exerciseWindow'></div>
+
+    const text = "Twenty years from now you will be more disappointed by the things that you didn't do than by the ones you did do. So throw off the bowlines. Sail away from the safe harbor. Catch the trade winds in your sails. Explore. Dream. Discover.";
+    const words = text.split(" ");
+
+    //State trackers
+    const [currentWordIndex, setCurrentWordIndex] = useState(0); // basically i = 0
+    const [started, setStarted] = useState(false);
+    const [finished, setFinished] = useState(false);
+
+    //The flashing words loop
+    useEffect(() => {
+        if (!started) return;
+
+        const interval = setInterval(() => {
+            setCurrentWordIndex((prevIndex) => {
+                if (prevIndex < words.length) {
+                    return prevIndex + 1;
+                } else {
+                    setFinished(true);
+                    return prevIndex;
+                }
+            });
             
-         </div>
-    )
-} 
+        }, 200 /*Adjust this for speed*/ );
+        
+        return () => clearInterval(interval); //Preventing memory leaks
+    }, [started, words.length]);
+
+
+    return (
+        <div className='mainContainer'>
+            <div className='exerciseWindow'>
+                <p className="singleWord">
+                    {started ? words[currentWordIndex] : "Press Go to begin"}
+                    {finished ? "― H. Jackson Brown Jr., P.S. I Love You" : ""}
+                </p>
+            </div>
+
+            <Row style={{ marginTop: '25px' }}>
+                <Col xs={12} md={2}>
+                    <Button className='subjectButtons' size="lg" style={{ backgroundColor: '#739900', borderColor: '#608000' }} onClick={() => setStarted(true)} disabled={started}>
+                        Go
+                    </Button>
+                </Col>
+            </Row>
+        </div>
+    );
+}
 
 export default Exercise;
