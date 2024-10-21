947df0074471f9e48d4acaa5272c58a032296796
tari9925 <tadas.riksas@mif.stud.vu.lt>
2024-10-21 12:46:25 +0300

    changed title reuired in update status

--- Backend/SpeedReaderAPI/DTOs/Article/Requests/ArticleUpdateRequest.cs
+++ Backend/SpeedReaderAPI/DTOs/Article/Requests/ArticleUpdateRequest.cs
@@ -4,12 +4,11 @@ using SpeedReaderAPI.Constants;
 
 public record ArticleUpdateRequest
 (
-	[Required(ErrorMessage = "Title is required.")]
     [StringLength(ValidationConstants.MaxTitleLength,
         MinimumLength = ValidationConstants.MinTitleLength,
         ErrorMessage = "Title must be between {2} and {1} characters.")]
 
-    string Title,
+    string? Title,
     string? CategoryTitle,
     List<int>? ParagraphIds
 );
