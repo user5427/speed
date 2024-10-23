678b8c425002243a157239ba2980796d51b59a64
Dariusspr <darius.spruogis@mif.stud.vu.lt>
2024-10-23 13:43:49 +0300

    Improve exceptionFilter return status readability.

--- Backend/SpeedReaderAPI/filters/ExceptionFilter.cs
+++ Backend/SpeedReaderAPI/filters/ExceptionFilter.cs
@@ -10,72 +10,54 @@ public class ExceptionFilter : IExceptionFilter
         
         context.Result = context.Exception switch 
         {
-         IndexOutOfRangeException => new ObjectResult(new ProblemDetails
+         IndexOutOfRangeException => new BadRequestObjectResult(new ProblemDetails
                     {
                         Title = "Index out of range",
                         Detail = $"{context.Exception.Message}",
                         Status = StatusCodes.Status400BadRequest,
                         Instance = context.HttpContext.Request.Path
                     }
-                )
-                {
-                    StatusCode = StatusCodes.Status400BadRequest
-                },
-            InvalidParagraphIdListException => new ObjectResult(new ProblemDetails
+                ),
+            InvalidParagraphIdListException => new BadRequestObjectResult(new ProblemDetails
                     {
                         Title = "Invalid paragraph id list",
                         Detail = "The provided list of paragraph IDs does not match the server's list",
                         Status = StatusCodes.Status400BadRequest,
                         Instance = context.HttpContext.Request.Path
                     }
-                )
-                {
-                    StatusCode = StatusCodes.Status400BadRequest
-                },
-             ResourceAlreadyExistsException => new ObjectResult(new ProblemDetails
+                ),
+             ResourceAlreadyExistsException => new BadRequestObjectResult(new ProblemDetails
                     {
                         Title = "Duplicate resource.",
                         Detail = $"{context.Exception.Message}",
                         Status = StatusCodes.Status400BadRequest,
                         Instance = context.HttpContext.Request.Path
                     }
-                )
-                {
-                    StatusCode = StatusCodes.Status400BadRequest
-                },
-             ResourceNotFoundException => new ObjectResult(new ProblemDetails
+                ),
+             ResourceNotFoundException => new NotFoundObjectResult(new ProblemDetails
                     {
                         Title = "Specified resource was not found.",
                         Detail = $"{context.Exception.Message}",
                         Status = StatusCodes.Status404NotFound,
                         Instance = context.HttpContext.Request.Path
                     }
-                )
-                {
-                    StatusCode = StatusCodes.Status404NotFound
-                },
-            ArgumentNullException => new ObjectResult(new ProblemDetails
+                ),
+            ArgumentNullException => new BadRequestObjectResult(new ProblemDetails
                     {
                         Title = "Missing arguments",
                         Detail = $"{context.Exception.Message}",
                         Status = StatusCodes.Status400BadRequest,
                         Instance = context.HttpContext.Request.Path
                     }
-                )
-                {
-                    StatusCode = StatusCodes.Status400BadRequest
-                },
-            UnsupportedFileFormatException => new ObjectResult(new ProblemDetails
+                ),
+            UnsupportedFileFormatException => new BadRequestObjectResult(new ProblemDetails
                     {
                         Title = "Unsupported file format",
                         Detail = $"Unsupported file extension",
                         Status = StatusCodes.Status400BadRequest,
                         Instance = context.HttpContext.Request.Path
                     }
-                )
-                {
-                    StatusCode = StatusCodes.Status400BadRequest
-                },
+                ),
             _ => new ObjectResult(new ProblemDetails
                 {
                     Title = "Internal Server Error",
@@ -87,7 +69,6 @@ public class ExceptionFilter : IExceptionFilter
                     StatusCode = StatusCodes.Status500InternalServerError
                 }
         };
-
         context.ExceptionHandled = true;
     }
 }
