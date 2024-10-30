44f89a2b13b3eefdf4e6e8b12cb1febff3400243
tari9925 <tadas.riksas@mif.stud.vu.lt>
2024-10-30 18:17:28 +0200

    added all basic tests

--- Backend/Tests/Unit/Tests/ParagraphTests/ParagraphServiceTests.cs
+++ Backend/Tests/Unit/Tests/ParagraphTests/ParagraphServiceTests.cs
@@ -9,15 +9,19 @@ using SpeedReaderAPI.DTOs.Article.Requests;
 using SpeedReaderAPI.Services.Impl;
 using SpeedReaderAPI.DTOs;
 using SpeedReaderAPI;
+using SpeedReaderAPI.DTOs.Paragraph.Requests;
+using SpeedReaderAPI.Exceptions;
+using SpeedReaderAPI.DTOs.Article.Responses;
 
 public class ParagraphServiceTests
 {
     private readonly ContextCreator _contextCreator;
     private readonly IMapper _mapper;
     private readonly ImageService _imageService;
+    private readonly ArticleService _articleService;
     private readonly ParagraphService _paragraphService;
     private readonly QuestionService _questionService;
-
+    private readonly ArticleResponse createdArticle;
     public ParagraphServiceTests()
     {
         // Create the context creator
@@ -38,7 +42,71 @@ public class ParagraphServiceTests
         _questionService = new QuestionService(context, _mapper, _imageService);
 
         _paragraphService = new ParagraphService(context, _mapper, _imageService, _questionService);
+
+        // Initialize ArticleService with mocks and context
+        _articleService = new ArticleService(context, _mapper, 
+                                             _imageService, 
+                                             _paragraphService);
+
+        // Initialize ArticleService with mock data
+        var request = new ArticleCreateRequest("Test Article", "Test Category"); 
+        createdArticle = _articleService.CreateArticle(request);
     }
 
-    
+    [Fact (DisplayName  = "Paragraph creating")]
+    public void CreateParagraph ()
+    {
+        var requestP = new ParagraphCreateRequest("Test Paragraph", createdArticle.Id, "Test Article");
+        
+        // Act
+        var result = _paragraphService.CreateParagraph(requestP);
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal("Test Paragraph", result.Title);
+        Assert.Equal(createdArticle.Id, result.ArticleId);
+    }
+
+    [Fact (DisplayName  = "Paragraph getting")]
+    public void GettingParagraph ()
+    {
+        var requestP = new ParagraphCreateRequest("Test Paragraph", createdArticle.Id, "Test Article");
+        var createdP = _paragraphService.CreateParagraph(requestP);
+
+        // Act
+        var result = _paragraphService.GetParagraph(createdP.Id);
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal("Test Paragraph", result.Title);
+        Assert.Equal(createdArticle.Id, result.ArticleId);
+    }
+
+    [Fact (DisplayName  = "Paragraph updating")]
+    public void UpdateParagraph ()
+    {
+        var requestP = new ParagraphCreateRequest("Test Paragraph", createdArticle.Id, "Test Article");
+        var createdP = _paragraphService.CreateParagraph(requestP);
+
+        var requestP2 = new ParagraphUpdateRequest("Test Paragraph 2", createdArticle.Id, "Test Article");
+        
+        // Act
+        var result = _paragraphService.UpdateParagraph(createdP.Id, requestP2);
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal("Test Paragraph 2", result.Title);
+        Assert.Equal(createdArticle.Id, result.ArticleId);
+    }
+
+    [Fact(DisplayName = "Paragraph deleting")]
+    public void DeleteParagraph()
+    {
+        var requestP = new ParagraphCreateRequest("Test Paragraph", createdArticle.Id, "Test Article");
+        var createdP = _paragraphService.CreateParagraph(requestP);
+        
+        // Act
+        _paragraphService.DeleteParagraph(createdP.Id);
+
+        // Assert
+        var exception = Assert.Throws<ResourceNotFoundException>(() => 
+            _paragraphService.GetParagraph(createdP.Id));
+    }
 }
--- Backend/Tests/Unit/Tests/QuestionTests/QuestionServiceTests.cs
+++ Backend/Tests/Unit/Tests/QuestionTests/QuestionServiceTests.cs
@@ -9,29 +9,28 @@ using SpeedReaderAPI.DTOs.Article.Requests;
 using SpeedReaderAPI.Services.Impl;
 using SpeedReaderAPI.DTOs;
 using SpeedReaderAPI;
+using SpeedReaderAPI.DTOs.Article.Responses;
+using SpeedReaderAPI.DTOs.Paragraph.Responses;
+using SpeedReaderAPI.DTOs.Paragraph.Requests;
+using SpeedReaderAPI.DTOs.Question.Requests;
+using SpeedReaderAPI.Exceptions;
 
 public class QuestionServiceTests
 {
     private readonly ContextCreator _contextCreator;
-    private readonly Mock<IMapper> _mockMapper;
     private readonly IMapper _mapper;
     private readonly ImageService _imageService;
+    private readonly ArticleService _articleService;
+    private readonly ParagraphService _paragraphService;
     private readonly QuestionService _questionService;
+    private readonly ArticleResponse createdArticle;
+    private readonly ParagraphResponse createdParagraph;
 
     public QuestionServiceTests()
     {
         // Create the context creator
         _contextCreator = new ContextCreator();
         var context = _contextCreator.CreateContext();
-
-        // Mock dependencies
-        _mockMapper = new Mock<IMapper>();
-        _mockMapper.Setup(m => m.Map<Article>(It.IsAny<ArticleCreateRequest>()))
-                   .Returns((ArticleCreateRequest request) => new Article 
-                   { 
-                       Title = request.Title, 
-                       CategoryTitle = request.CategoryTitle 
-                   });
 
          // Set up AutoMapper with the actual mappings
         var mapperConfig = new MapperConfiguration(cfg =>
@@ -45,6 +44,79 @@ public class QuestionServiceTests
         // _mockMapper.Object
 
         _questionService = new QuestionService(context, _mapper, _imageService);
+
+        _paragraphService = new ParagraphService(context, _mapper, _imageService, _questionService);
+
+        // Initialize ArticleService with mocks and context
+        _articleService = new ArticleService(context, _mapper, 
+                                             _imageService, 
+                                             _paragraphService);
+
+        // Initialize ArticleService with mock data
+        var request = new ArticleCreateRequest("Test Article", "Test Category");
+        createdArticle = _articleService.CreateArticle(request);
+
+        // Initialize ParagraphService with mock data
+        var requestP = new ParagraphCreateRequest("Test Paragraph", createdArticle.Id, "Test Article");
+        createdParagraph = _paragraphService.CreateParagraph(requestP);
+    }
+
+    [Fact (DisplayName  = "Question creating")]
+    public void CreateQuestion ()
+    {
+        // Arrange
+        var request = new QuestionCreateRequest(createdParagraph.Id, "Test Question", ["answer 1", "answer 2"], 0);
+        
+        // Act
+        var result = _questionService.CreateQuestion(request);
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal("Test Question", result.QuestionText);
+        Assert.Equal(createdParagraph.Id, result.ParagraphId);
+    }
+
+    [Fact (DisplayName  = "Question getting")]
+    public void GettingQuestion ()
+    {
+        // Arrange
+        var request = new QuestionCreateRequest(createdParagraph.Id, "Test Question", ["answer 1", "answer 2"], 0);
+        var created = _questionService.CreateQuestion(request);
+        
+        // Act
+        var result = _questionService.GetQuestion(created.Id);
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal("Test Question", result.QuestionText);
+        Assert.Equal(createdParagraph.Id, result.ParagraphId);
+    }
+
+    [Fact (DisplayName  = "Question updating")]
+    public void UpdateQuestion ()
+    {
+        // Arrange
+        var request = new QuestionCreateRequest(createdParagraph.Id, "Test Question", ["answer 1", "answer 2"], 0);
+        var created = _questionService.CreateQuestion(request);
+        
+        // Act
+        var request2 = new QuestionUpdateRequest(createdParagraph.Id, "Test Question 2", ["answer 1", "answer 2"], 0);
+        var result = _questionService.UpdateQuestion(created.Id, request2);
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal("Test Question 2", result.QuestionText);
+        Assert.Equal(createdParagraph.Id, result.ParagraphId);
+    }
+
+    [Fact(DisplayName = "Question deleting")]
+    public void DeleteQuestion()
+    {
+        // Arrange
+        var request = new QuestionCreateRequest(createdParagraph.Id, "Test Question", ["answer 1", "answer 2"], 0);
+        var created = _questionService.CreateQuestion(request);
+        
+        // Act
+        _questionService.DeleteQuestion(created.Id);
+        var exception = Assert.Throws<ResourceNotFoundException>(() => 
+            _questionService.GetQuestion(created.Id));
     }
 
     
