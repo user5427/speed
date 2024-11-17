263c813153134fb4c6f6681b3151049dc81d8e25
tari9925 <tadas.riksas@mif.stud.vu.lt>
2024-11-17 17:19:49 +0200

    Added category component

--- ClientApplication/react-app/src/.controllers/.MainControllersExport.js
+++ ClientApplication/react-app/src/.controllers/.MainControllersExport.js
@@ -1,5 +1,7 @@
 import { ArticleController } from "./articleController";
 import { ParagraphController } from "./paragraphController";
 import { QuestionController } from "./questionController";
+import { UserController } from "./userController";
+import { CategoryController } from "./categoryController";
 
-export { ArticleController, ParagraphController, QuestionController };
+export { ArticleController, ParagraphController, QuestionController, UserController, CategoryController };
