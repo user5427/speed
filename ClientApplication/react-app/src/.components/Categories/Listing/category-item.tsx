d2829f840f2c19475c1b20dd18cc0d4069cc013b
tari9925 <tadas.riksas@mif.stud.vu.lt>
2024-11-18 14:44:10 +0200

    Add CategoryItem component and TypeScript configuration

---
+++ ClientApplication/react-app/src/.components/Categories/Listing/category-item.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { Button, Col, Row } from 'react-bootstrap';
+import "../../../styles/Articles/articleItemStyle.css"; // stylesheet
+import { MdModeEdit } from "react-icons/md";
+import { useTranslation } from 'react-i18next'; 
+import { FaBookOpenReader } from "react-icons/fa6";
+
+interface CategoryItemProps {
+    data: {
+        title: string;
+        categoryTitle: string;
+    };
+    settings?: {
+        showSelectButton?: boolean;
+        showDeleteButton?: boolean;
+        showEditButton?: boolean;
+        showPlayButton?: boolean;
+    };
+    selectThis?: () => void;
+    deleteThis?: () => void;
+    editThis?: () => void;
+    playThis?: () => void;
+}
+
+
+const CategoryItem: React.FC<CategoryItemProps> = (props) => {
+
+    const { t } = useTranslation();
+    const { settings, selectThis, deleteThis, editThis } = props;
+
+    return (
+        <>
+            <div className="article-item">
+                <Row className="row">
+                    <Col xs={12} md={10} className="col col-12 col-md-10">
+                        <h2 className="wrap-title">{props.data.title}</h2>
+                    </Col>
+                    <Col xs={12} md={2} className="col col-12 col-md-2">
+                        <p className="wrap-category">{props.data.categoryTitle}</p>
+                    </Col>
+                </Row>
+
+                <div style={{ display: 'flex', gap: '10px' }}> {/* Add gap between buttons */}
+
+                    {settings && settings.showSelectButton && (
+                        <div> {/* Flex for each button container */}
+                            <Button onClick={selectThis} className='buttons amber'>
+                                {t('commonUIelements.select')}
+                            </Button>
+                        </div>
+                    )}
+
+                    {settings && settings.showDeleteButton && (
+                        <div>
+                            <Button onClick={deleteThis} className='buttons red'>
+                                {t('commonUIelements.delete')}
+                            </Button>
+                        </div>
+                    )}
+
+                    {settings && settings.showEditButton && (
+                        <div>
+                            <Button onClick={editThis} className='buttons lightBlue'>
+                                <MdModeEdit className="icons" /> {t('commonUIelements.edit')}
+                            </Button>
+                        </div>
+                    )}
+
+                </div>
+            </div>
+        </>
+    );
+
+}
