5f5662398d7fbb9baab9b09853e60ae4d1e41f53
Dariusspr <darius.spruogis@mif.stud.vu.lt>
2024-12-04 19:11:45 +0200

    add user to category migrations

---
+++ Backend/SpeedReaderAPI/Data/Migrations/20241204171122_add_user_questions.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SpeedReaderAPI.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_user_questions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "UserId",
+                table: "Category",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Category_UserId",
+                table: "Category",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Category_User_UserId",
+                table: "Category",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Category_User_UserId",
+                table: "Category");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Category_UserId",
+                table: "Category");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Category");
+        }
+    }
+}
