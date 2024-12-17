7eccc21b7e29d32710a2d2178d17d213d7465cc4
tari9925 <tadas.riksas@mif.stud.vu.lt>
2024-12-17 19:40:47 +0200

    Update Docker configuration to use standard HTTP/HTTPS ports and remove deprecated settings

--- Backend/SpeedReaderAPI/Program.cs
+++ Backend/SpeedReaderAPI/Program.cs
@@ -229,11 +229,11 @@ try
     
 
     // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
-    {
+    // if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
+    // {
         app.UseSwagger();
         app.UseSwaggerUI();
-    }
+    // }
 
     app.UseHttpsRedirection();
 
