653c39183b66a7ece4a42127510d1733e9773c91
tari9925 <tadas.riksas@mif.stud.vu.lt>
2024-12-19 14:05:22 +0200

    Improve error message handling in sendAPIError function to limit detail length

--- ClientApplication/react-app/src/.controllers/.dataProcessingHelpers/AuthorizedProcessing/auth-status-helper.ts
+++ ClientApplication/react-app/src/.controllers/.dataProcessingHelpers/AuthorizedProcessing/auth-status-helper.ts
@@ -1,7 +1,7 @@
 export const sendAPIError = (error: any, type?: string) => {
     var errorMessage;
     if (error.respose?.data?.title !== undefined || error.response?.data?.detail !== undefined || error.response?.status !== undefined) {
-        errorMessage = `Title: ${error.response.data.title || "N/A"}` + ` Details: ${error.response.data.detail || "N/A"}` + ` Status code: ${error.response?.status || "N/A"}`;
+        errorMessage = `Title: ${error.response.data.title || "N/A"}` + ` Details: ${error.response.data.detail && error.response.data.detail.length < 200 || "N/A"}` + ` Status code: ${error.response?.status || "N/A"}`;
     } else if (error.message !== undefined) {
         errorMessage = error.message;
     } else {
