936a01e1f0d64aeb8070400935f016c7cf27ee28
Erelix <adrian.klimasevski@gmail.com>
2024-12-19 02:01:53 +0200

    added article extra info loading from actual database

--- ClientApplication/react-app/src/pages/Exercise/articleData.js
+++ ClientApplication/react-app/src/pages/Exercise/articleData.js
@@ -1,10 +1,6 @@
   export const exerciseInfo = {
-    author: 'Erin Garcia de Jesús',
-    source: 'https://shorturl.at/4H1md',
-    publisher: 'ScienceNews',
     avgReadingSpeed: 238,
     worldRecordWPM: 25000,
-    usersWPM: 1000,
-    addedBy: 'me'
+    usersWPM: 1000
   };
   
